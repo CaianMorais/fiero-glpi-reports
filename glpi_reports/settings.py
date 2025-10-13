dbab2629f08df00dfe1ebda85a88c798fcdf39f6
CaianMorais <caianqm@gmail.com>
2025-10-13 16:37:59 -0400

    modo debug no .env

--- glpi_reports/settings.py
+++ glpi_reports/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG_MODE')
 
-ALLOWED_HOSTS = ['10.67.117.201']
+ALLOWED_HOSTS = ['10.67.118.128']
 
 
 # Application definition
